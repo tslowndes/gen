ec065ef9d63be2706236ddbcd033b295d77e1f45
Unknown <tom@cdsat.co.uk>
2017-10-02 14:15:20 +0100

    writing_proof uses dataframes.to_csv

--- writing.py
+++ writing.py
@@ -3,11 +3,10 @@ import os
 import csv
 import numpy as np
 from math import atan, degrees
-from dist import dist
-from find_dir import *
 import getpass
 import os
 import pandas as pd
+from latlon_util import find_dist2, find_dist3, find_dir
 
 def get_file_number(path):
     inds = []
@@ -41,32 +40,6 @@ def write_results(Flock, Evaluate, config, repeat):
 
     pd.DataFrame(dfresults).to_csv(filename)
 
-def write_solo(AUV, config):
-    filename = 'results/sim_%03i' % config.no + '/solo.csv'
-    if getpass.getuser() == 'tsl1g12':
-        f = open(filename, 'wb')
-    else:
-        f = open(filename, 'w', newline='')
-    wr = csv.writer(f)
-
-    headers = ['t']
-    for i in range(config.swarm_size):
-        headers.append('x%i' % i)
-        headers.append('y%i' % i)
-        headers.append('z%i' % i)
-    wr.writerow(headers)
-    temp = [[] for i in range(config.run_time)]
-    for i in range(config.run_time):
-        temp[i].append(i)
-        for pos in AUV.log.loc_pos[i]:
-            temp[i].append(pos[0])
-            temp[i].append(pos[1])
-            temp[i].append(pos[2])
-
-    temp = np.array(temp)
-    wr.writerows(temp)
-    f.close()
-
 def  write_proof(AUV, config, fn = ' '):
     if config.sim_type == 1:
         if config.sim_sub_type == 1:
@@ -82,122 +55,70 @@ def  write_proof(AUV, config, fn = ' '):
         else:
             filename = fn
 
-    if getpass.getuser() == 'tsl1g12' and os.path.exists('/noc/users/tsl1g12'):
-        f = open(filename, 'wb')
-    else:
-        f = open(filename, 'w', newline='')
+    dfresults = {'t':[i for i in range(config.run_time)],
+                 'lat':AUV.log.lat,
+                 'lon':AUV.log.lon,
+                 'z':AUV.log.z,
+                 'lat_demand':AUV.log.lat_demand,
+                 'lon_demand':AUV.log.lon_demand,
+                 'z_demand':AUV.log.z_demand,
+                 'state':AUV.log.state,
+                 'time_uw':AUV.log.time_uw,
+                 'v':AUV.log.v,
+                 'v_demand':AUV.log.v_demand,
+                 'yaw':AUV.log.yaw,
+                 'yaw_demand':AUV.log.yaw_demand,
+                 'pitch':AUV.log.pitch,
+                 'pitch_demand':AUV.log.pitch_demand,
+                 'sat_times':AUV.log.sat_time_stamps + [0 for i in range(config.run_time - lan(AUV.log.sat_time_stamps))]}
 
-    wr = csv.writer(f)
-    headers = ['t']
+    dist_to_wp, v_calc, yr_calc, yd_calc, pd_calc, pr_calc = np.zeros((len(AUV.log.x),6))
 
-    headers.append('x')
-    headers.append('y')
-    headers.append('z')
-
-    headers.append('wp_x')
-    headers.append('wp_y')
-    headers.append('wp_z')
-    headers.append('dist_to_wp')
-    headers.append('state')
-    headers.append('time_uw')
-
-    headers.append('v')
-    headers.append('v_demand')
-    headers.append('v_calc')
-
-    headers.append('yaw')
-    headers.append('yaw_demand')
-    headers.append('yaw_demand_calc')
-    headers.append('yaw_rate_calc')
-
-    headers.append('pitch')
-    headers.append('pitch_demand')
-    headers.append('pitch_demand_calc')
-    headers.append('pitch_rate_calc')
-
-    headers.append('sat_times')
-
-    wr.writerow(headers)
-    start_ind = 0
-    temp = [[] for i in range(len(AUV.log.x))]
     for i in range(len(AUV.log.x)):
-        # time
-        temp[i].append(i)
-        # position
-        temp[i].append(AUV.log.x[i])
-        temp[i].append(AUV.log.y[i])
-        temp[i].append(AUV.log.z[i])
-        # current waypoint
-        temp[i].append(AUV.log.x_demand[i])
-        temp[i].append(AUV.log.y_demand[i])
-        temp[i].append(AUV.log.z_demand[i])
-        temp[i].append(dist([AUV.log.x[i], AUV.log.y[i], AUV.log.z[i]], [AUV.log.x_demand[i], AUV.log.y_demand[i], AUV.log.z_demand[i]], 2))
+        dist_to_wp[i] = find_dist3((AUV.log.lon[i], AUV.log.lat[i], AUV.log.z[i]), (AUV.log.lon_demand[i], AUV.log.lat_demand[i], AUV.log.z_demand[i]))
 
-        # time spent underwater
-        temp[i].append(AUV.log.state[i])
-        temp[i].append(AUV.log.time_uw[i])
-
-        # velocity
-        temp[i].append(AUV.log.v[i])
-        temp[i].append(AUV.log.v_demand[i])
-        if i == 0:
-            temp[i].append(AUV.log.v[0])
+        if i ==0:
+            v_calc[i] = 0
+            yr_calc[i] = 0
+            pr_calc[i] = 0
         else:
-            # Calculated velocity
-            temp[i].append(dist([AUV.log.x[i], AUV.log.y[i], AUV.log.z[i]], [AUV.log.x[i - 1], AUV.log.y[i - 1], AUV.log.z[i - 1]], 3) / 0.5)
+            v_calc[i] = find_dist3((AUV.log.lon[i], AUV.log.lat[i], AUV.log.z[i]), (AUV.log.lon[i - 1], AUV.log.lat[i - 1], AUV.log.z[i - 1])) / config.time_step
 
-        #### Yaw
-        temp[i].append(AUV.log.yaw[i])
-        temp[i].append(AUV.log.yaw_demand[i])
-        #### Calculated yaw demand
-        if AUV.log.x[i] != AUV.log.x_demand[i] or AUV.log.y[i] != AUV.log.y_demand[i]:
-            temp[i].append(find_dir(AUV.log.x[i], AUV.log.y[i], AUV.log.x_demand[i], AUV.log.y_demand[i]))
-        else:
-            temp[i].append(temp[i][-1])
-
-        if i == 0:
-            temp[i].append(0)
-        else:
             #### Calculated Yaw Rate
             if AUV.log.yaw[i-1] > 350 and AUV.log.yaw[i] < 10:
-                print
-                temp[i].append((AUV.log.yaw[i] - (AUV.log.yaw[i-1] - 360)) / 0.5)
+                yr_calc[i] = (AUV.log.yaw[i] - (AUV.log.yaw[i-1] - 360)) / config.run_time
             elif AUV.log.yaw[i-1] < 10 and AUV.log.yaw[i] > 350:
-                temp[i].append(((AUV.log.yaw[i] - 360) - AUV.log.yaw[i-1]) / 0.5)
+                yr_calc[i] = (AUV.log.yaw[i] - 360) - AUV.log.yaw[i-1]) /  config.run_time
             else:
-                temp[i].append((AUV.log.yaw[i] - AUV.log.yaw[i-1])/0.5)
+                yr_calc[i] = (AUV.log.yaw[i] - AUV.log.yaw[i-1])/ config.run_time
 
-        # Pitch
-        temp[i].append(AUV.log.pitch[i])
-        temp[i].append(AUV.log.pitch_demand[i])
+            # Calculated Pitch Rate
+            pr_calc[i] = (AUV.log.pitch[i] - AUV.log.pitch[i-1])/ config.run_time
+
+        # if AUV.log.lon[i] != AUV.log.lon_demand[i] or AUV.log.lat[i] != AUV.log.lat_demand[i]:
+        yd_calc[i] = find_dir((AUV.log.lon[i], AUV.log.lat[i]), (AUV.log.lon_demand[i], AUV.log.lat_demand[i]))
+        # else:
+            # yd_calc.append(temp[i][-1])
+
 
         # Pitch demand calc
-        dist_xy = dist([AUV.log.x[i], AUV.log.y[i]], [AUV.log.x_demand[i], AUV.log.y_demand[i]], 2)
-        if dist_xy == 0 and AUV.log.z[i] == AUV.log.z_demand[i]:
-            p_demand = 0
-        elif dist_xy == 0 and AUV.log.z[i] != AUV.log.z_demand[i]:
-            p_demand = AUV.config.max_pitch
+        dist_xy = find_dist2((AUV.log.lon[i], AUV.log.lat[i]), (AUV.log.lon_demand[i], AUV.log.lat_demand[i]))
+        # if dist_xy == 0 and AUV.log.z[i] == AUV.log.z_demand[i]:
+        #     pd_calc[i] = 0
+        if dist_xy == 0 and AUV.log.z[i] != AUV.log.z_demand[i]:
+            pd_calc[i] = AUV.config.max_pitch
         else:
-            p_demand = degrees(atan(( AUV.log.z_demand[i] - AUV.log.z[i] ) / dist_xy))
-            if abs(p_demand) > AUV.config.max_pitch:
-                p_demand = (abs(p_demand) / p_demand) * AUV.config.max_pitch
-        temp[i].append(-1*p_demand)
-        # Pitch rate calc
-        if i == 0:
-            temp[i].append(0)
-        else:
-            # Calculated Pitch Rate
-            temp[i].append((AUV.log.pitch[i] - AUV.log.pitch[i-1])/0.5)
+            pd_calc[i] = degrees(atan(( AUV.log.z_demand[i] - AUV.log.z[i] ) / dist_xy))
+            if abs(pd_calc[i]) > AUV.config.max_pitch:
+                pd_calc[i] = (abs(pd_calc[i]) / pd_calc[i]) * AUV.config.max_pitch
+        pd_calc[i] = -1*pd_calc[i]
 
+    dfresults.update({'pitch_demand_calc':pd_calc})
+    dfresults.update({'pitch_rate_calc': pr_calc})
+    dfresults.update({'dist_to_wp': dist_to_wp})
+    dfresults.update({'yaw_demand_calc': yd_calc})
+    dfresults.update({'yaw_rate_calc': yr_calc})
+    dfresults.update({'v_calc': v_calc})
 
-        if i < len(AUV.log.sat_time_stamps):
-            temp[i].append(AUV.log.sat_time_stamps[i])
-        else:
-            temp[i].append(0)
+    pd.DataFrame(dfresults).to_csv(filename)
 
-    temp = np.array(temp)
-
-    for i in range(len(temp)):
-        wr.writerow(temp[i])
-
-    f.close()
