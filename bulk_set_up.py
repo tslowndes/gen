936eca2d4fb6f0191dbe080a8d8b4b1324a3a8a9
Unknown <tom@cdsat.co.uk>
2017-10-03 16:54:01 +0100

    bulk runs

--- bulk_set_up.py
+++ bulk_set_up.py
@@ -14,10 +14,10 @@ def main_set_up():
         config.comms = 0
         config.dive_depth = 0
         config.sim_sub_type = 1
-        config.t_uw = 240
-        config.repeats = 1
-        config.run_time = 10000
-        config.swarm_size = 10
+        config.t_uw = 120
+        config.repeats = 10
+        config.run_time = 5000
+        config.swarm_size = 4
         config.seed = [0, 1, 2, 3, 4, 5, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 24, 25, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57]
         config.desired_d = d
         filename = '../Voronoi/3D/config/sim_config_%03i.csv' % sim_no
