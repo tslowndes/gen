d965cb0afdc2826a61e740689eef23a106b95cf2
Unknown <tom@cdsat.co.uk>
2017-12-05 10:15:38 +0000

    sim_config_old

--- Config_class.py
+++ Config_class.py
@@ -27,3 +27,30 @@ class sim_config:
 
     def __init__(self, config_file):
         self.read_config(config_file)
+
+class sim_config_old:
+    def read_config(self, config_file):
+        with open(config_file, 'r+') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                if row[0] == 'seed':
+                    temp = []
+                    for item in row[1][1:-1].split(','):
+                        temp.append(int(item))
+                    setattr(self,row[0], temp)
+
+                elif (row[0] == 'start_box_se_lat' or row[0] == 'start_box_nw_lat'
+                      or row[0] == 'start_box_nw_lon' or row[0] == 'start_box_nw_lon'):
+                    setattr(self, row[0], np.float64(row[1]))
+
+                else:
+                    try:
+                        setattr(self, row[0], int(row[1]))
+                    except:
+                        try:
+                            setattr(self, row[0], float(row[1]))
+                        except:
+                            setattr(self, row[0], str(row[1]))
+
+    def __init__(self, config_file):
+        self.read_config(config_file)
